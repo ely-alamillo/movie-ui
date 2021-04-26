2a27873604714c5441195af3330ced3045e564fb
Ely Alamillo <ely0013@gmail.com>
2021-04-25 21:27:29 -0500

    feat(layout): add layout and header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ export const Header = () => {
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
           <span className="ml-3 text-xl">Reel Cinema</span>
-        </a>        
+        </a>
       </div>
     </header>
   )
